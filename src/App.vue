bf0f9fe512e8f4c7b59ee9a6452b7e8797d9927b
FabioGafforelli <fabio.gafforelli@edu.univ-fcomte.fr>
2022-11-27 11:54:02 +0100

    manifest ?

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
+<link rel="manifest" href="/public/manifest.webmanifest">
 
 
   <RouterView />
 </template>
 
 <script>
+
 import IconLogo from "./components/icones/Logo.vue";
 import { FilmIcon } from "@heroicons/vue/outline";
 export default {
